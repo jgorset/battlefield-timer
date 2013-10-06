db05c0f8ce68da4ef92949664b6179b6ef68eafd
Johannes Gorset <jgorset@gmail.com>
2013-10-06 12:13:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BattlefieldTimer::Application.config.secret_key_base = 'd5264e64a9a29ca8b604f308aa00b902a07e209d27c360525a3617c867d6a5052d9a25b04a3b56829eeba816edf8a1274caad7adcd55c97b88b91eb3c5c943f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BattlefieldTimer::Application.config.session_store :cookie_store, key: '_battlefield-timer_session'
