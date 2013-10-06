da97904d7f5b18dc11b98aed1305dc920e431616
Johannes Gorset <jgorset@gmail.com>
2013-10-06 12:35:24 +0200

    Prefer SASS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ BattlefieldTimer::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Prefer SASS over SCSS
+  config.sass.preferred_syntax = :sass
 end
